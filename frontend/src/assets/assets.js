ae1a7c449870ea18a0937100370bf1fb546f3963
Rodolfo Lorca Campos <r.e.lorca@gmail.com>
2024-06-24 18:44:04 -0400

    first commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,408 @@
+import basket_icon from "./basket_icon.png";
+import logo from "./logo.png";
+import header_img from "./header_img.png";
+import search_icon from "./search_icon.png";
+import menu_1 from "./menu_1.png";
+import menu_2 from "./menu_2.png";
+import menu_3 from "./menu_3.png";
+import menu_4 from "./menu_4.png";
+import menu_5 from "./menu_5.png";
+import menu_6 from "./menu_6.png";
+import menu_7 from "./menu_7.png";
+import menu_8 from "./menu_8.png";
+
+import food_1 from "./food_1.png";
+import food_2 from "./food_2.png";
+import food_3 from "./food_3.png";
+import food_4 from "./food_4.png";
+import food_5 from "./food_5.png";
+import food_6 from "./food_6.png";
+import food_7 from "./food_7.png";
+import food_8 from "./food_8.png";
+import food_9 from "./food_9.png";
+import food_10 from "./food_10.png";
+import food_11 from "./food_11.png";
+import food_12 from "./food_12.png";
+import food_13 from "./food_13.png";
+import food_14 from "./food_14.png";
+import food_15 from "./food_15.png";
+import food_16 from "./food_16.png";
+import food_17 from "./food_17.png";
+import food_18 from "./food_18.png";
+import food_19 from "./food_19.png";
+import food_20 from "./food_20.png";
+import food_21 from "./food_21.png";
+import food_22 from "./food_22.png";
+import food_23 from "./food_23.png";
+import food_24 from "./food_24.png";
+import food_25 from "./food_25.png";
+import food_26 from "./food_26.png";
+import food_27 from "./food_27.png";
+import food_28 from "./food_28.png";
+import food_29 from "./food_29.png";
+import food_30 from "./food_30.png";
+import food_31 from "./food_31.png";
+import food_32 from "./food_32.png";
+
+import add_icon_white from "./add_icon_white.png";
+import add_icon_green from "./add_icon_green.png";
+import remove_icon_red from "./remove_icon_red.png";
+import app_store from "./app_store.png";
+import play_store from "./play_store.png";
+import linkedin_icon from "./linkedin_icon.png";
+import facebook_icon from "./facebook_icon.png";
+import twitter_icon from "./twitter_icon.png";
+import cross_icon from "./cross_icon.png";
+import selector_icon from "./selector_icon.png";
+import rating_starts from "./rating_starts.png";
+import profile_icon from "./profile_icon.png";
+import bag_icon from "./bag_icon.png";
+import logout_icon from "./logout_icon.png";
+import parcel_icon from "./parcel_icon.png";
+
+export const assets = {
+  logo,
+  basket_icon,
+  header_img,
+  search_icon,
+  rating_starts,
+  add_icon_green,
+  add_icon_white,
+  remove_icon_red,
+  app_store,
+  play_store,
+  linkedin_icon,
+  facebook_icon,
+  twitter_icon,
+  cross_icon,
+  selector_icon,
+  profile_icon,
+  logout_icon,
+  bag_icon,
+  parcel_icon,
+};
+
+export const menu_list = [
+  {
+    menu_name: "Salad",
+    menu_image: menu_1,
+  },
+  {
+    menu_name: "Rolls",
+    menu_image: menu_2,
+  },
+  {
+    menu_name: "Deserts",
+    menu_image: menu_3,
+  },
+  {
+    menu_name: "Sandwich",
+    menu_image: menu_4,
+  },
+  {
+    menu_name: "Cake",
+    menu_image: menu_5,
+  },
+  {
+    menu_name: "Pure Veg",
+    menu_image: menu_6,
+  },
+  {
+    menu_name: "Pasta",
+    menu_image: menu_7,
+  },
+  {
+    menu_name: "Noodles",
+    menu_image: menu_8,
+  },
+];
+
+export const food_list = [
+  {
+    _id: "1",
+    name: "Greek salad",
+    image: food_1,
+    price: 12,
+    description:
+      "Greek salad consists of sliced cucumbers, tomatoes, green bell pepper, red onion, olives, and feta cheese.",
+    category: "Salad",
+  },
+  {
+    _id: "2",
+    name: "Veg salad",
+    image: food_2,
+    price: 18,
+    description:
+      "Vegetable salads may be marinated or sauced mixtures of raw or cooked vegetables. They are commonly based on tomatoes, green beans, cucumbers, beets, and mushrooms.",
+    category: "Salad",
+  },
+  {
+    _id: "3",
+    name: "Clover Salad",
+    image: food_3,
+    price: 16,
+    description:
+      "White clover seed has long been used as a natural remedy for a variety of aliments, including respiratory problems, skin disorders, and digestive issues.",
+    category: "Salad",
+  },
+  {
+    _id: "4",
+    name: "Chicken Salad",
+    image: food_4,
+    price: 24,
+    description:
+      "Chicken salad is rich in lean protein and a decent source of iron.",
+    category: "Salad",
+  },
+  {
+    _id: "5",
+    name: "Lasagna Rolls",
+    image: food_5,
+    price: 14,
+    description:
+      "The proportion of cheese, meat, and sauce in between these perfectly cooked noodles is absolutely perfect. And the seasonings in the cheese mixture takes it over the top.",
+    category: "Rolls",
+  },
+  {
+    _id: "6",
+    name: "Peri Peri Rolls",
+    image: food_6,
+    price: 12,
+    description:
+      "Peri Peri rolls are a delightful fusion of spicy, tangy, and savory flavors.",
+    category: "Rolls",
+  },
+  {
+    _id: "7",
+    name: "Chicken Rolls",
+    image: food_7,
+    price: 20,
+    description:
+      "Chicken rolls are a versatile and delicious meal that can be enjoyed as a snack, lunch, or even dinner. ",
+    category: "Rolls",
+  },
+  {
+    _id: "8",
+    name: "Veg Rolls",
+    image: food_8,
+    price: 15,
+    description:
+      "Vegetable rolls are a delicious and healthy option that can be enjoyed as a snack, appetizer, or even a light meal. ",
+    category: "Rolls",
+  },
+  {
+    _id: "9",
+    name: "Ripple Ice Cream",
+    image: food_9,
+    price: 14,
+    description:
+      "Ripple ice cream is a delightful treat featuring a creamy base with swirls of flavorful sauce, often fruit-based, creating a marbled effect.",
+    category: "Deserts",
+  },
+  {
+    _id: "10",
+    name: "Fruit Ice Cream",
+    image: food_10,
+    price: 22,
+    description:
+      "Fruit ice cream is a refreshing and delicious treat that combines the natural sweetness and flavor of fruits with a creamy base.",
+    category: "Deserts",
+  },
+  {
+    _id: "11",
+    name: "Jar Ice Cream",
+    image: food_11,
+    price: 10,
+    description:
+      "Making ice cream in a jar is a fun and easy way to create a personalized treat without needing an ice cream maker. ",
+    category: "Deserts",
+  },
+  {
+    _id: "12",
+    name: "Vanilla Ice Cream",
+    image: food_12,
+    price: 12,
+    description:
+      "Classic Vanilla ice cream at Tomato's is an incredible and delicious experience.",
+    category: "Deserts",
+  },
+  {
+    _id: "13",
+    name: "Chicken Sandwich",
+    image: food_13,
+    price: 12,
+    description:
+      "A chicken sandwich is a classic and satisfying meal that can be enjoyed for lunch or dinner.",
+    category: "Sandwich",
+  },
+  {
+    _id: "14",
+    name: "Vegan Sandwich",
+    image: food_14,
+    price: 18,
+    description:
+      "Feel free to customize the sandwich filling with your favorite vegetables or add-ins like grated carrots, bell peppers, or pickles.",
+    category: "Sandwich",
+  },
+  {
+    _id: "15",
+    name: "Grilled Sandwich",
+    image: food_15,
+    price: 16,
+    description:
+      "Grilled sandwiches are perfect for a quick and satisfying meal any time of the day.",
+    category: "Sandwich",
+  },
+  {
+    _id: "16",
+    name: "Bread Sandwich",
+    image: food_16,
+    price: 24,
+    description:
+      "A bread sandwich, also known as a the simple, yet comforting snack made with just a few basic ingredients.",
+    category: "Sandwich",
+  },
+  {
+    _id: "17",
+    name: "Cup Cake",
+    image: food_17,
+    price: 14,
+    description: "Cupcakes are perfect for any occasion or celebration. ",
+    category: "Cake",
+  },
+  {
+    _id: "18",
+    name: "Vegan Cake",
+    image: food_18,
+    price: 12,
+    description:
+      "Enjoy this moist and flavorful vegan cake as a delightful treat for any occasion.",
+    category: "Cake",
+  },
+  {
+    _id: "19",
+    name: "Butterscotch Cake",
+    image: food_19,
+    price: 20,
+    description:
+      "If desired, drizzle additional butterscotch sauce over the top of the cake for extra flavor and decoration.",
+    category: "Cake",
+  },
+  {
+    _id: "20",
+    name: "Sliced Cake",
+    image: food_20,
+    price: 15,
+    description:
+      "Sliced cake, it could simply mean a cake that has been cut into slices for serving. ",
+    category: "Cake",
+  },
+  {
+    _id: "21",
+    name: "Garlic Mushroom ",
+    image: food_21,
+    price: 14,
+    description:
+      "Garlic mushrooms make for a flavorful and versatile dish that can be enjoyed as a side, appetizer, or even a topping for pasta or steak.",
+    category: "Pure Veg",
+  },
+  {
+    _id: "22",
+    name: "Fried Cauliflower",
+    image: food_22,
+    price: 22,
+    description:
+      "Fried cauliflower makes for a delicious and crispy snack or side dish.",
+    category: "Pure Veg",
+  },
+  {
+    _id: "23",
+    name: "Mix Veg Pulao",
+    image: food_23,
+    price: 10,
+    description:
+      "Mix veg pulao is a flavorful and aromatic rice dish packed with assorted vegetables and spices.",
+    category: "Pure Veg",
+  },
+  {
+    _id: "24",
+    name: "Rice Zucchini",
+    image: food_24,
+    price: 12,
+    description:
+      "Zucchini rice is a delicious and healthy dish that combines fluffy rice with tender zucchini and flavorful seasonings.",
+    category: "Pure Veg",
+  },
+  {
+    _id: "25",
+    name: "Cheese Pasta",
+    image: food_25,
+    price: 12,
+    description:
+      "Cheese pasta, also known as macaroni and cheese, is a classic and comforting dish loved by many.",
+    category: "Pasta",
+  },
+  {
+    _id: "26",
+    name: "Tomato Pasta",
+    image: food_26,
+    price: 18,
+    description:
+      "Tomato pasta, also known as pasta al pomodoro, is a classic Italian dish featuring pasta tossed in a flavorful tomato sauce.",
+    category: "Pasta",
+  },
+  {
+    _id: "27",
+    name: "Creamy Pasta",
+    image: food_27,
+    price: 16,
+    description:
+      "Creamy pasta dishes are beloved for their comforting and satisfying qualities, making them popular choices for cozy dinners or special occasions.",
+    category: "Pasta",
+  },
+  {
+    _id: "28",
+    name: "Chicken Pasta",
+    image: food_28,
+    price: 24,
+    description:
+      "Chicken pasta is a delicious and hearty dish that combines tender pieces of chicken with pasta in a flavorful sauce.",
+    category: "Pasta",
+  },
+  {
+    _id: "29",
+    name: "Buttter Noodles",
+    image: food_29,
+    price: 14,
+    description:
+      "Butter noodles are wonderfully simple on their own, but you can also add other ingredients to enhance the flavor.",
+    category: "Noodles",
+  },
+  {
+    _id: "30",
+    name: "Veg Noodles",
+    image: food_30,
+    price: 12,
+    description:
+      "Veg noodles, also known as vegetable noodles or stir-fried noodles, are a popular and flavorful dish made with a combination of colorful vegetables and noodles.",
+    category: "Noodles",
+  },
+  {
+    _id: "31",
+    name: "Somen Noodles",
+    image: food_31,
+    price: 20,
+    description:
+      "Somen noodles are thin Japanese noodles made from wheat flour, similar to spaghetti but thinner in diameter.",
+    category: "Noodles",
+  },
+  {
+    _id: "32",
+    name: "Cooked Noodles",
+    image: food_32,
+    price: 15,
+    description:
+      "Feel free to get creative and experiment with different ingredients and flavor combinations to create your own delicious dish.",
+    category: "Noodles",
+  },
+];
